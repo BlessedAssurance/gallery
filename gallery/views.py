ede34cdb23e3689c1d1a63b6fa3fccbd6b585ad2
BA <engineervinceblair@gmail.com>
2020-05-23 23:56:32 +0300

    create function for displaying days' glalery

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render, HttpResponse
+import datetime as dt 
 
 # Create your views here.
 def welcome (request):
     return HttpResponse('BA PHOTO GALLERY')
+
+def gallery_today(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
