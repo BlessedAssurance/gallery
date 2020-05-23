3a07435e9fb37a7cb063081baa77c7b722a3a4fa
BA <engineervinceblair@gmail.com>
2020-05-24 02:16:20 +0300

    create blocks for defining templates

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.gallery_today,name='galleryToday')
+    url('^today/$',views.gallery_today,name='galleryToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastNews')
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, Http404, redirect
 import datetime as dt 
 
 # Create your views here.
 def welcome (request):
-    return HttpResponse('BA PHOTO GALLERY')
+    return render(request, 'welcome.html')
 
 def gallery_today(request):
     date = dt.date.today()
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'all-photos/today-photos.html', {"date": date,})
+
+
+
+def past_days_gallery(request,past_date):
+    # View Function to present news from past days:
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+    assert False
+
+    if date == dt.date.today():
+        return redirect(gallery_today)
+
+    return render(request, 'all-photos/past-photos.html', {"date": date})
