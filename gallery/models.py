093af825e3c523848dfe83e90aa1e2d396bbdf17
BA <engineervinceblair@gmail.com>
2020-05-25 01:04:06 +0300

    created model Category to show categories of images

--- gallery/models.py
+++ gallery/models.py
@@ -32,3 +32,19 @@ def __str__(self):
         return f"{self.locs}"
 
 
+#CATEGORY
+class Category(models.Model):
+    categories = (
+        ('Flowers','Flowers'),
+        ('Articles','Articles'),
+        ('Animals','Animals'),
+        ('People','People')
+    )
+    cate = models.CharField(max_length = 255, choices = categories)
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
+
+    def __str__(self):
+        return f"{self.cate}"
