999972878c501799402c72eea48bdceca85c6d91
BA <engineervinceblair@gmail.com>
2020-05-25 01:02:47 +0300

    created Images model

--- gallery/models.py
+++ gallery/models.py
@@ -12,39 +12,3 @@ class Images (models.Model):
     class Meta:
         ordering = ['name']
 
-#LOCATION
-class Location(models.Model):
-    locations=(
-        ('Kenya','Kenya'),
-        ('Uganda','Uganda'),
-        ('Paris','Paris'),
-        ('London','London'),
-        ('Mexico','Mexico'),
-        ('India','India'),
-    )
-    locs = models.CharField(max_length = 255, choices = locations)
-
-    class Meta:
-        verbose_name_plural = 'Location'
-    
-
-def __str__(self):
-        return f"{self.locs}"
-
-
-#CATEGORY
-class Category(models.Model):
-    categories = (
-        ('Flowers','Flowers'),
-        ('Articles','Articles'),
-        ('Animals','Animals'),
-        ('People','People')
-    )
-    cate = models.CharField(max_length = 255, choices = categories)
-    class Meta:
-        verbose_name_plural = 'categories'
-
-
-
-    def __str__(self):
-        return f"{self.cate}"
