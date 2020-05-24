16200888eaac8cc1c529a4d5db41b78200ffeb5f
BA <engineervinceblair@gmail.com>
2020-05-25 01:01:38 +0300

    created test for setup

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from .models import Images
 # Create your tests here.
+
+# set up method
+def setUp(self):
+    self.flowergirl= Images(name = 'flowergirl', description='Girl holding a flower')
+# Testing  instance
+def test_instance(self):
+    self.assertTrue(isinstance(self.flowergirl,Images))
