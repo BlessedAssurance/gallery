4e9e0321d57890dfded54a7dabef557c73fdc883
BA <engineervinceblair@gmail.com>
2020-05-25 21:44:13 +0300

    updated class Images model class

--- gallery/models.py
+++ gallery/models.py
@@ -5,12 +5,49 @@ from django.db import models
 # IMAGES
 class Images (models.Model):
     name= models.CharField(max_length =30)
-    description = models.CharField(max_length =100)
+    image=models.ImageField(upload_to ='images/')
+    description = models.TextField()
+    image_location = models.ForeignKey('Location', on_delete=models.CASCADE,)
+    image_category = models.ForeignKey('Category', on_delete=models.CASCADE,)
+
+    @classmethod
+    def search_image(cls,category):
+        images = cls.objects.filter(image_category__name =category )
+        
+        return images
+    
+    @classmethod 
+    def get_all_images(cls):
+        images=cls.objects.all()
+        return images
+    
+    @classmethod
+    def save_image(self):
+        self.save()
+        
+    @classmethod
+    def delete_image(self):
+        self.delete()
+    
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image.value)
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = cls.objects.filter(id= id).all()
+        return image
+
+    
+    @classmethod
+    def filter_by_location(cls,location):
+        images_location = cls.objects.filter(image_location_name=location)
+        return images_location
 
     def __str__(self):
         return self.name
-    class Meta:
-        ordering = ['name']
+    # class Meta:
+    #     ordering = ['name']
 
 #LOCATION
 class Location(models.Model):
@@ -26,6 +63,17 @@ class Location(models.Model):
 
     class Meta:
         verbose_name_plural = 'Location'
+    @classmethod
+    def save_location(self):
+        self.save()
+    
+    @classmethod 
+    def delete_location(self):
+        self.delete()
+        
+    @classmethod
+    def update_locs(cls, id, new_locs):
+        cls.objects.filter(id=id).update(locs=new_locs)
     
 
 def __str__(self):
@@ -44,6 +92,17 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'categories'
 
+    @classmethod
+    def save_category(self):
+        self.save()
+        
+    @classmethod
+    def delete_category(self):
+        self.delete()
+        
+    @classmethod
+    def update_cate(cls, id, new_cate):
+        cls.objects.filter(id=id).update(cate=new_cate)
 
 
     def __str__(self):
--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
 from . import views
+from .views import SearchResultsListView,SearchLocationListView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url(r'^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.gallery_today,name='galleryToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastNews')
+    url('',views.home,name='home'),
+    url('search/', SearchResultsListView.as_view(), name='search_results'),
+    url('search/', SearchLocationListView.as_view(), name='search_results'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
