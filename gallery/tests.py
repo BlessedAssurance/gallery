f62728fd78b79829ab3aff35f9a3867885b8af9b
BA <engineervinceblair@gmail.com>
2020-05-26 11:57:59 +0300

    Testcase for Category to test instance of categories

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,19 +1,25 @@
 from django.test import TestCase
-from .models import Images
+from .models import Images, Category
 # Create your tests here.
 
 # set up method
-def setUp(self):
-    self.flowergirl= Images(name = 'flowergirl', description='Girl holding a flower')
-# Testing  instance
-def test_instance(self):
-    self.assertTrue(isinstance(self.flowergirl,Images))
+class ImageTest(TestCase):
+    def setUp(self):
+     self.flowergirl= Images(name = 'flowergirl', description='Girl holding a flower')
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.flowergirl,Images))
 
-def test_instance(self):
-        self.assertTrue(isinstance(self.image, Image))
-    # Testing Save Method
-
-def test_save_method(self):
+    def test_save_method(self):
         self.image.save_image()
         images = Images.objects.all()
         self.assertTrue(len(images) > 0)
+
+class CategoryTest(TestCase):
+    def setUp(self):
+        self.category = Category(name = 'flowers', id=1)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category)) 
+
+    
