397c2e0de0c4b09cca4a6039c2f0213dc02ae507
BA <engineervinceblair@gmail.com>
2020-05-26 03:38:50 +0300

    added test for save method

--- gallery/tests.py
+++ gallery/tests.py
@@ -8,3 +8,12 @@ def setUp(self):
 # Testing  instance
 def test_instance(self):
     self.assertTrue(isinstance(self.flowergirl,Images))
+
+def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+    # Testing Save Method
+
+def test_save_method(self):
+        self.image.save_image()
+        images = Images.objects.all()
+        self.assertTrue(len(images) > 0)
