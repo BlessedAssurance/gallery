ee2ac0eb64e968ad31ef28be8e01a158ebefb001
BA <engineervinceblair@gmail.com>
2020-05-27 16:35:48 +0300

    edit .env file

--- gallery/admin.py
+++ gallery/admin.py
@@ -5,3 +5,12 @@ from .models import Images, Location, Category
 admin.site.register(Images)
 admin.site.register(Location)
 admin.site.register(Category)
+
+
+
+# export DB_NAME= 'gallery'
+# export DB_USER= 'vincent'
+# export DB_PASSWORD= '29484510'
+# export SECRET_KEY= 'e!g$6q44=v%67^6bh1of!vymkt+9kyf*_t(u4vt99-lh(c5krl'
+# DEBUG = True
+
