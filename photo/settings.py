eecb6cedfe7ab8968ca7a08bd919c39f2634d0d2
BA <engineervinceblair@gmail.com>
2020-05-28 01:10:17 +0300

    set database name

--- photo/settings.py
+++ photo/settings.py
@@ -22,13 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
-
+SECRET_KEY = 'e!g$6q44=v%67^6bh1of!vymkt+9kyf*_t(u4vt99-lh(c5krl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -85,11 +84,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_NAME', 'postgres'),
         'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', '29484510'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
         'TEST': {
-            'NAME': 'test_photo'
+            'NAME': 'test_gallery'
         }
     },
 }
